f643d787015f0773be073eff67b1b736eb2f9e82
bublis <serickablom@mail.ru>
2014-12-21 14:25:24 +0300

    c4ityvanie mac var2

--- e1000_main.c
+++ e1000_main.c
@@ -1,3 +1,4 @@
+ 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pci.h>
@@ -8,27 +9,21 @@ char e1000_driver_name[] = "e1000";
 	PCI_DEVICE(PCI_VENDOR_ID_INTEL, device_id)}
 
 static const struct pci_device_id e1000_pci_tbl[] = {
-	INTEL_E1000_ETHERNET_DEVICE(0x1015),
+	INTEL_E1000_ETHERNET_DEVICE(0x100E),
 	{0,}
 };
 
 MODULE_DEVICE_TABLE(pci, e1000_pci_tbl);
 
-static unsigned char skel_get_revision(struct pci_dev *dev)
-{
-	u8 revision;
 
-	pci_read_config_byte(dev, PCI_REVISION_ID, &revision);
-	return revision;
-}
+static int bars;
 
 static int probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
 	u8 __iomem *hw_addr;
-	unsigned int mac;
+	unsigned int mac[6];
 	
-	int err;
-	int bars;
+	int err, i;
 
 	bars = pci_select_bars(pdev, IORESOURCE_MEM | IORESOURCE_IO);
 	err = pci_enable_device(pdev);
@@ -42,11 +37,13 @@ static int probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	hw_addr = pci_ioremap_bar(pdev, 0);
 	if (!hw_addr)
 		goto err_ioremap;
-	mac = readl(hw_addr);
-	pr_warn("mac=%u", mac);
-	
-	if (skel_get_revision(pdev) == 0x42)
-		return -ENODEV;
+	for (i = 0; i < 6; i++)
+		mac[i] = readb(hw_addr);
+
+	printk("mac=");
+	for (i = 0; i < 6; i++)
+		printk("%x:", mac[i]);
+
 	return 0;
 
 err_ioremap:
@@ -56,11 +53,13 @@ err_pci_reg:
 	return err;
 }
 
-static void remove(struct pci_dev *dev)
+static void remove(struct pci_dev *pdev)
 {
 	/* clean up any allocated resources and stuff here.
 	 * like call release_region();
 	 */
+	pci_release_selected_regions(pdev, bars);
+	pci_disable_device(pdev);
 }
 
 static struct pci_driver pci_driver = {
@@ -72,15 +71,17 @@ static struct pci_driver pci_driver = {
 
 static int __init pci_skel_init(void)
 {
+	printk("itstalled e100_main\n");
 	return pci_register_driver(&pci_driver);
 }
 
 static void __exit pci_skel_exit(void)
 {
+	printk("removed e100_main\n");
 	pci_unregister_driver(&pci_driver);
 }
 
 MODULE_LICENSE("GPL");
 
 module_init(pci_skel_init);
-module_exit(pci_skel_exit);
+module_exit(pci_skel_exit); 
--- e1000_main.c~
+++ e1000_main.c~
@@ -1,15 +1,18 @@
-#include <linux/config.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/init.h>
 
+char e1000_driver_name[] = "e1000";
+#define INTEL_E1000_ETHERNET_DEVICE(device_id) {\
+	PCI_DEVICE(PCI_VENDOR_ID_INTEL, device_id)}
 
-static struct pci_device_id ids[] = {
-	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_82801AA_3), },
-	{ 0, }
+static const struct pci_device_id e1000_pci_tbl[] = {
+	INTEL_E1000_ETHERNET_DEVICE(0x1015),
+	{0,}
 };
-MODULE_DEVICE_TABLE(pci, ids);
+
+MODULE_DEVICE_TABLE(pci, e1000_pci_tbl);
 
 static unsigned char skel_get_revision(struct pci_dev *dev)
 {
@@ -19,18 +22,38 @@ static unsigned char skel_get_revision(struct pci_dev *dev)
 	return revision;
 }
 
-static int probe(struct pci_dev *dev, const struct pci_device_id *id)
+static int probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
-	/* Do probing type stuff here.  
-	 * Like calling request_region();
-	 */
-	pci_enable_device(dev);
+	u8 __iomem *hw_addr;
+	unsigned int mac;
 	
-	if (skel_get_revision(dev) == 0x42)
+	int err;
+	int bars;
+
+	bars = pci_select_bars(pdev, IORESOURCE_MEM | IORESOURCE_IO);
+	err = pci_enable_device(pdev);
+	if (err)
+		return err;
+
+	err = pci_request_selected_regions(pdev, bars, e1000_driver_name);
+	if (err)
+		goto err_pci_reg;
+
+	hw_addr = pci_ioremap_bar(pdev, 0);
+	if (!hw_addr)
+		goto err_ioremap;
+	mac = readl(hw_addr);
+	pr_warn("mac=%u", mac);
+	
+	if (skel_get_revision(pdev) == 0x42)
 		return -ENODEV;
-
-
 	return 0;
+
+err_ioremap:
+	pci_release_selected_regions(pdev, bars);
+err_pci_reg:
+	pci_disable_device(pdev);
+	return err;
 }
 
 static void remove(struct pci_dev *dev)
@@ -42,7 +65,7 @@ static void remove(struct pci_dev *dev)
 
 static struct pci_driver pci_driver = {
 	.name = "pci_skel",
-	.id_table = ids,
+	.id_table = e1000_pci_tbl,
 	.probe = probe,
 	.remove = remove,
 };
